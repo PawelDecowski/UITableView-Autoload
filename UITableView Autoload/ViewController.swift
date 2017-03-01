8316e28a4e45a49b9c10b479dd16943564e8d4ee
Pawel Decowski <pawel.decowski@gmail.com>
2017-03-01 17:00:28 +0000

    Initial Commit

---
+++ UITableView Autoload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableView Autoload
+//
+//  Created by Pawel Decowski on 01/03/2017.
+//  Copyright © 2017 Relish Media Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
